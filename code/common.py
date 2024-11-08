309a9af9310a439d8371fd398e2e4cfc986baf73
Umar Taufiqulhakim <umar@clumio.com>
2024-11-08 16:51:52 +0900

    Update ebs list backups to use clumioapi

---
+++ code/common.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+def get_sort_and_ts_filter(
+    direction: str, start_day_offset: int, end_day_offset: int
+) -> tuple[str, str]:
+    """Get the sort and the timestamp filter."""
+    current_timestamp = datetime.datetime.now(datetime.UTC)
+    end_timestamp = current_timestamp - datetime.timedelta(days=end_day_offset)
+    end_timestamp_str = end_timestamp.strftime('%Y-%m-%d') + 'T23:59:59Z'
+
+    start_timestamp = current_timestamp - datetime.timedelta(days=start_day_offset)
+    start_timestamp_str = start_timestamp.strftime('%Y-%m-%d') + 'T00:00:00Z'
+    if direction == 'after':
+        sort = 'start_timestamp'
+        ts_filter = '"start_timestamp": {"$gt":"' + start_timestamp_str + '", "$lte":"' + end_timestamp_str + '"}'
+    else:
+        sort = '-start_timestamp'
+        ts_filter = '"start_timestamp": {"$lte":"' + end_timestamp_str + '"}'
+
+    return sort, ts_filter
+
+
+def get_total_list(function, api_filter, sort):
+    """Get the list of all items
+
+    Args:
+        function: must be list API function call with pagination feature.
+        api_filter: the filter applied to the list API.
+        sort: the sort applied to the list API.
+    """
+    start = 1
+    total_list = []
+    while True:
+        response = function(filter=api_filter, sort=sort, start=start)
+        total_list.extend(response.embedded.items)
+        if response.total_pages_count <= start:
+            break
+        start += 1
+    return total_list
