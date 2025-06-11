844641da63e5e48fa43c8a8511a00a9478f5f7e3
Chris Waago <52174445+cwaago@users.noreply.github.com>
2025-06-11 10:44:31 -0700

    CQ-6820. Add source_asset_types filter (#40)
    Added `source_asset_types` filter to JSON input. Use this input to find
    tag IDs to add to the list_function filter.

--- code/clumio_bulk_list_assets.py
+++ code/clumio_bulk_list_assets.py
@@ -16,10 +16,10 @@ if TYPE_CHECKING:
     from aws_lambda_powertools.utilities.typing import LambdaContext
     from common import EventsTypeDef
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
-def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, Any]:  # noqa: PLR0912 PLR0915
+def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, Any]:  # noqa: PLR0911, PLR0912 PLR0915
     """Handle the lambda functions to list of the assets given the env and resource type."""
     # Retrieve and validate the inputs.
     clumio_token: str | None = events.get('clumio_token', None)
@@ -28,7 +28,8 @@ def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, A
     region: dict = events.get('region', {})
     region_name: str = region.get('region', '')
     env_id: str = region.get('environment_id', '')
-    asset_meta_status: dict = events.get('asset_meta_status', None)
+    source_asset_types: dict | None = events.get('source_asset_types', None)
+    asset_meta_status: dict | None = events.get('asset_meta_status', None)
 
     # If clumio bearer token is not passed as an input read it from the AWS secret.
     if not clumio_token:
@@ -37,6 +38,12 @@ def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, A
             return {'status': status, 'msg': msg}
         clumio_token = msg
 
+    # Check if source_asset_types was provided.
+    asset_tags: dict = {}
+    if source_asset_types and resource_type in source_asset_types:
+        asset_tags = source_asset_types[resource_type]['asset_tags']
+
+    # Verify asset_meta_status was provided.
     if not asset_meta_status:
         return {'status': 422, 'msg': 'asset_meta_status is a required input.'}
     else:
@@ -44,14 +51,16 @@ def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, A
         backup_status = asset_meta_status.get('backup_status_in', None)
         is_deleted = asset_meta_status.get('deleted_status_in', None)
 
+    # Verify at least one asset_meta_status filter was provided.
     if any(value is not None for value in (protection_status, backup_status, is_deleted)):
         pass
     else:
         return {
             'status': 422,
             'msg': 'Some asset status filtering is needed.\
-                protection_status, backup_status_in and deleted_status_in ',
+                protection_status_in, backup_status_in or deleted_status_in',
         }
+
     # Initiate the Clumio API client.
     base_url = common.parse_base_url(base_url)
     config = configuration.Configuration(
@@ -85,7 +94,32 @@ def lambda_handler(events: EventsTypeDef, context: LambdaContext) -> dict[str, A
     else:
         return {'status': 401, 'msg': f'Resource type {resource_type} is not supported.'}
 
+    # Add tags filter.
+    if asset_tags:
+        logger.info('Search for asset tags %s...', asset_tags)
+        tag_ids = []
+        for tag_key, tag_value in asset_tags.items():
+            # Get the tag from Clumio inventory matching the specified tag value.
+            tag_filter = {'value': {'$contains': tag_value}}
+            _, tags = client.aws_environment_tags_v1.list_aws_environment_tags(
+                env_id, filter=json.dumps(tag_filter)
+            )
+            # Get the Clumio tag ID.
+            clumio_tag_ids = []
+            for tag in tags.embedded.items:
+                # Ensure exact key/value match as filter only allows $contains.
+                if tag.value == tag_value and tag.key == tag_key:
+                    logger.info('Found tag {%s:%s} with ID: %s', tag_key, tag_value, tag.id)
+                    clumio_tag_ids.append(tag.id)
+            # Bail out if the tag was not found.
+            if not clumio_tag_ids:
+                logger.error('Tag not found: {%s:%s}', tag_key, tag_value)
+                return {'status': 404, 'msg': f'Tag not found: {tag_key}:{tag_value}'}
+            tag_ids += clumio_tag_ids
+        list_filter['tags.id'] = {'$all': tag_ids}
+
     try:
+        logger.info('Filter: %s', list_filter)
         logger.info('List all %s assets...', resource_type)
         assets_list = common.get_total_list(
             function=list_function, api_filter=json.dumps(list_filter)
